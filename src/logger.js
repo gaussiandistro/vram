bb18851f17c6ce51932e3d86d5ba43f2143da3b8
Ian Rodrigues <ian.rodrigues.knight@gmail.com>
2026-09-10 09:32:29 +0700

    Temporary check to log errors

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,4 @@
 export function logError(message, error) {
-  if (import.meta.env.DEV) {
-    // eslint-disable-next-line no-console
-    console.error(message, error);
-  }
+  // eslint-disable-next-line no-console
+  console.error(message, error);
 }
